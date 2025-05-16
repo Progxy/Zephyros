e38012d97c46a2b06e9ff3751896293b0956c890
TheProgxy <theprogxy@gmail.com>
2025-05-16 14:41:29 +0200

    Add support for Windows

--- server/zrest.c
+++ server/zrest.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 
-#include "../zephyros.h"
+#ifdef _WIN32
+	#include <winsock2.h>
+	#include <ws2tcpip.h>
+#else
+	#include <unistd.h>
+	#include <sys/socket.h>
+	#include <netinet/in.h>
+#endif //_WIN32
+
+/* #include "../zephyros.h" */
 
 #define FALSE 0
 #define TRUE  1
@@ -76,43 +82,111 @@ static void parse_json_request(const char* query, const int size) {
 	return;
 }
 
-int main (void) {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock < 0) {
+#ifdef _WIN32
+
+typedef SOCKET socket_zt;
+#define close_socket closesocket
+#define SOCK_CLEANUP() WSACleanup()
+#define IS_INVALID_SOCK(s) ((s) == INVALID_SOCKET)
+
+static int init_server(SOCKET* sock) {
+	WSADATA wsaData = {0};
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        perror("WSAStartup failed");
+        return -1;
+    }
+
+    *sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (*sock == INVALID_SOCKET) {
+        perror("Socket creation failed");
+        WSACleanup();
+        return -1;
+    }
+
+    int opt = 1;
+    if (setsockopt(*sock, SOL_SOCKET, SO_REUSEADDR, (const char *)&opt, sizeof(opt)) == SOCKET_ERROR) {
+        perror("setsockopt failed");
+        closesocket(*sock);
+        WSACleanup();
+        return -1;
+    }
+
+    struct sockaddr_in serverAddr = {0};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = htonl(SERVER_ADDR);
+    serverAddr.sin_port = htons(SERVER_PORT);
+    
+	if (bind(*sock, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        perror("bind failed");
+        closesocket(*sock);
+        WSACleanup();
+        return -1;
+    }
+
+    if (listen(*sock, SOMAXCONN) == SOCKET_ERROR) {
+        perror("listen failed");
+        closesocket(*sock);
+        WSACleanup();
+        return -1;
+    }
+
+	return 0;
+}
+
+#else
+
+typedef int socket_zt;
+#define close_socket close
+#define SOCK_CLEANUP()
+#define IS_INVALID_SOCK(sock) ((sock) < 0)
+
+static int init_server(int* sock) {
+    *sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (*sock < 0) {
 		perror("Failed to create the socket");
-		return 1;
+		return -1;
 	}
 	
 	int val = 1;
-	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+	if (setsockopt(*sock, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val)) < 0) {
+		perror("Failed to set sock option, because");
+		close(*sock);
+		return -1;
+	}
 	
 	struct sockaddr_in addr = {0};
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(SERVER_PORT);
 	addr.sin_addr = (struct in_addr) { .s_addr = htonl(SERVER_ADDR) };
 	
-	if (bind(sock, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
+	if (bind(*sock, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
 		perror("Failed to bind the socket");
-		return 1;
+		return -1;
 	}
 
-	if (listen(sock, 1) < 0) {
+	if (listen(*sock, 1) < 0) {
 		perror("Failed to listen to the socket");
-		return 1;
+		return -1;
 	}
 
+	return 0;
+}
+
+#endif //_WIN32
+
+int main (void) {
 	const char response[] = "HTTP/1.1 204 No Content\r\n"
 	"Access-Control-Allow-Origin: *\r\n"
 	"Access-Control-Allow-Methods: POST, OPTIONS\r\n"
 	"Access-Control-Allow-Headers: content-type\r\n"
 	"Content-Length: 0\r\n"
-	"Connection: close\r\n"
+	"Connection: keep-alive\r\n"
 	"\r\n";
 
 	const char data_response[] = "HTTP/1.1 200 OK\r\n"
 	"Access-Control-Allow-Origin: *\r\n"
 	"Content-Length: 939\r\n"
-	"Connection: close\r\n"
+	"Connection: keep-alive\r\n"
 	"Content-Type: application/json\r\n"
 	"\r\n"
 	"["
@@ -124,16 +198,33 @@ int main (void) {
     "{\"city\":\"Perugia\",\"type\":1,\"temperature\":\"32°\",\"humidity\":\"69%\",\"pressure\":\"1015 hPa\",\"wind_velocity\":69,\"wind_direction\":\"12° NW\"},"
     "{\"city\":\"Perugia\",\"type\":2,\"temperature\":\"-45°\",\"humidity\":\"80%\",\"pressure\":\"1420 hPa\",\"wind_velocity\":42,\"wind_direction\":\"71° W\"}"
     "]";
+	
+	socket_zt sock = 0;
+	if (init_server(&sock) < 0) {
+		printf("An error occurred while initializing the server.\n");
+		return 1;
+	}
 
 	printf("Listening on http://localhost:6969 ...\n\n");
 	
 	unsigned int i = 0;
 	while(TRUE) {
-		int client_sock = 0;
-		socklen_t client_addr_size = 0;
 		struct sockaddr_in client_addr = {0};
-		if ((client_sock = accept(sock, (struct sockaddr*) &client_addr, &client_addr_size)) < 0) {
+		socklen_t client_addr_size = sizeof(client_addr);
+		
+		socket_zt client_sock = accept(sock, (struct sockaddr*) &client_addr, &client_addr_size);
+		if (IS_INVALID_SOCK(client_sock)) {	
+		#ifdef _WIN32
+			int err = WSAGetLastError();
+			char *msg = NULL;
+			FormatMessageA(
+				FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+				NULL, err, 0, (LPSTR)&msg, 0, NULL);
+			printf("Error message: %s\n", msg);
+			LocalFree(msg);
+		#else 
 			perror("Failed to accept incoming connection");
+		#endif //_WIN32
 			return 1;
 		}
 		
@@ -182,7 +273,7 @@ int main (void) {
 
 		printf("Sent response back (%d)!\n", res_len);
 
-		close(client_sock);
+		close_socket(client_sock);
 
 		printf("-------------------\n");
 		printf("Connection closed.\n");
@@ -191,7 +282,9 @@ int main (void) {
 		i = (i + 1) % 2;
 	}
 		
-	close(sock);
+	close_socket(sock);
+	SOCK_CLEANUP();
 
    	return 0;
 }
+
