f1c35f769ccc4d5d7db309bbe7be7c1978ab9417
TheProgxy <theprogxy@gmail.com>
2025-05-18 16:48:58 +0200

    Extend support with dynamic responses, as well as some leak check fixes

--- server/zrest.c
+++ server/zrest.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #ifdef _WIN32
 	#include <winsock2.h>
@@ -37,6 +38,16 @@ static int str_tok(const char* str, const char* delim) {
     return -1;
 }
 
+static int str_n_tok(const char* str, const char* delim, const size_t size) {
+     for (size_t i = 0, j = 0; i < size; ++i) {
+         if (delim[j] == str[i]) {
+            if ((j + 1) == str_len(delim)) return i - j;
+            j++;
+         } else if (j > 0) i -= j, j = 0;
+    }
+    return -1;
+}
+
 #define CAST_PTR(ptr, type) ((type*) (ptr))
 
 static void* mem_cpy(void* dest, const void* src, size_t size) {
@@ -46,29 +57,6 @@ static void* mem_cpy(void* dest, const void* src, size_t size) {
 	while (size--) *dp++ = *sp++;
 	return dest;
 }
-
-static void* mem_move(void* dest, const void* src, size_t size) {
-    if (dest == NULL || src == NULL || size == 0) return NULL;
-    
-	size_t s = size;
-	unsigned char* temp = (unsigned char*) calloc(size, sizeof(unsigned char));
-	if (temp == NULL) {
-		printf("Failed to allocate the temp buffer for mem_move.\n");
-		return NULL;
-	}
-	
-	unsigned char* dp = CAST_PTR(dest, unsigned char);
-	unsigned char* sp = (unsigned char*) src;
-
-	while (size--) *temp++ = *sp++;
-	temp -= s, size = s;
-	while (s--)	*dp++ = *temp++;
-	
-	free(temp - size);
-    
-    return dest;
-}
-
 
 #define mem_set(ptr, value, size)    mem_set_var(ptr, value, size, sizeof(unsigned char))
 #define mem_set_32(ptr, value, size) mem_set_var(ptr, value, size, sizeof(unsigned int))
@@ -83,10 +71,10 @@ static void mem_set_var(void* ptr, long long int value, size_t size, size_t val_
 
 #define IS_A_NUM(chr) ((chr >= '0') && (chr <= '9'))
 #define CHR_TO_NUM(chr) chr - '0'
-static long long int str_to_num(const char* str, const char* delim) {
+static long long int str_to_num(const char* str, const char* delim, const int size) {
 	long long int ret = 0;
-	ssize_t len = str_tok(str, delim);
-	if (len == -1) len = str_len(str);
+	ssize_t len = str_n_tok(str, delim, size);
+	if (len == -1) len = size;
 
 	for (ssize_t i = 0; i < len; ++i) {
 		if (IS_A_NUM(str[i])) {
@@ -130,23 +118,22 @@ static int check_request_type(const char* buffer) {
 	return -1;
 }
 
-
-static int parse_json_request(const char* query, char** city, unsigned char* day) {
-	printf("query: %s\n\n", query);
+static int parse_json_request(const char* query, const int size, char** city, char** key, unsigned char* day) {
+	printf("query: %*.s\n\n", size, query);
 
 	int pos = 0;
-	if ((pos = str_tok(query, "city")) < 0) {
+	if ((pos = str_n_tok(query, "city", size)) < 0) {
 		printf("No field \"city\" found in the JSON.\n");
 		return -1;
 	}
 
 	int len = 0;
-	if ((len = str_tok(query + pos + 7, "\"")) < 0) {
+	if ((len = str_n_tok(query + pos + 7, "\"", size - pos - 7)) < 0) {
 		printf("missing \" in JSON.\n");
 		return -1;
 	}
 	
-	*city = calloc(len, sizeof(char));
+	*city = calloc(len + 1, sizeof(char));
 	if (*city == NULL) {
 		printf("Failed to allocate buffer.\n");
 		return -1;
@@ -154,13 +141,33 @@ static int parse_json_request(const char* query, char** city, unsigned char* day
 	
 	mem_cpy(*city, query + pos + 7, len);
 
-	if ((pos = str_tok(query, "day")) < 0) {
+	pos = 0;
+	if ((pos = str_n_tok(query, "key", size)) < 0) {
+		printf("No field \"key\" found in the JSON.\n");
+		return -1;
+	}
+
+	len = 0;
+	if ((len = str_n_tok(query + pos + 6, "\"", size - pos - 6)) < 0) {
+		printf("missing \" in JSON.\n");
+		return -1;
+	}
+	
+	*key = calloc(len + 1, sizeof(char));
+	if (*key == NULL) {
+		printf("Failed to allocate buffer.\n");
+		return -1;
+	}
+	
+	mem_cpy(*key, query + pos + 6, len);
+
+	if ((pos = str_n_tok(query, "day", size)) < 0) {
 		printf("No field \"day\" found in the JSON.\n");
 		SAFE_FREE(*city);
 		return -1;
 	}
 	
-	long long int val = str_to_num(query + pos + 6, "\"");
+	long long int val = str_to_num(query + pos + 6, "\"", size - pos - 6);
 	if (val < 0) {
 		printf("missing \" in JSON.\n");
 		SAFE_FREE(*city);
@@ -172,20 +179,94 @@ static int parse_json_request(const char* query, char** city, unsigned char* day
 	return 0;
 }
 
-static int check_content_size(const char* buffer) {
-	int pos = str_tok(buffer, "Content-Length:");
+static int check_content_size(const char* buffer, const int size) {
+	int pos = str_n_tok(buffer, "Content-Length:", size);
 	if (pos < 0) {
 		printf("ERROR: no content-length specifier found.\n");
 		return -1;
 	}
 
-	long long int content_len = str_to_num(buffer + pos + 16, "\r\n");
+	long long int content_len = str_to_num(buffer + pos + 16, "\r\n", size - pos - 16);
 	if (content_len < 0) {
 		printf("ERROR: No value found at the end of the content field.\n");
 		return -1;
 	}
 
 	return content_len;
+}
+
+typedef struct Weather {
+	unsigned char type;
+	int temperature;
+	unsigned int pressure;
+	unsigned int humidity;
+	unsigned int wind_velocity;
+	unsigned int wind_direction;
+} Weather;
+
+static Weather gen_weather(void) {
+	Weather w = {0};
+	w.type = (rand() % 5) + 1;
+	w.temperature = (rand() % 50) * ((rand() % 2) * 1);
+	w.pressure = (rand() % 256) + 1000;
+	w.humidity = rand() % 100;
+	w.wind_velocity = rand() % 100;
+	w.wind_direction = rand() % 180;
+	return w;
+}
+
+static int generate_response(char** response, char* city, unsigned char day) {
+	unsigned int content_capacity = 256;
+	char* content = (char*) calloc(content_capacity, sizeof(char));
+	if (content == NULL) {
+		printf("Failed to allocate the content buffer.\n");
+		return -1;
+	}
+	
+	mem_set(content, '[', sizeof(char));
+
+	unsigned int content_size = 1;
+
+	for (unsigned char i = 0; i <= day; ++i) {
+		content_capacity += 256;
+		
+		content = (char*) realloc(content, content_capacity);
+		if (content == NULL) {
+			printf("Failed to reallocate the content buffer.\n");
+			return -1;
+		}
+		
+		mem_set(content + content_size, 0, content_capacity - content_size);
+	
+		Weather weather = gen_weather();
+		content_size += snprintf(content + content_size, content_capacity - content_size, "{\"city\":\"%s\",\"type\":%u,\"temperature\":\"%d°\",\"humidity\":\"%u%%\",\"pressure\":\"%u hPa\",\"wind_velocity\":%u,\"wind_direction\":\"%u° SE\"}", city, weather.type, weather.temperature, weather.humidity, weather.pressure, weather.wind_velocity, weather.wind_direction);
+		
+		if (i < day) mem_set(content + content_size, ',', sizeof(char)), content_size++;
+	}
+	
+	mem_set(content + content_size, ']', sizeof(char));
+	content_size++;
+
+	*response = (char*) calloc(256 + content_size, sizeof(char));
+	if (*response == NULL) {
+		SAFE_FREE(content);
+		printf("Failed to allocate response buffer.\n");
+		return -1;
+	}
+	
+	int response_size = snprintf(*response, 256 + content_size, "HTTP/1.1 200 OK\r\n"
+	"Access-Control-Allow-Origin: *\r\n"
+	"Content-Length: %d\r\n"
+	"Connection: keep-alive\r\n"
+	"Content-Type: application/json\r\n"
+	"\r\n"
+	"%s", content_size, content);
+	
+	printf("Response(%d):\n'%s'\n", response_size, *response);
+	
+	SAFE_FREE(content);
+
+	return response_size;
 }
 
 #ifdef _WIN32
@@ -280,30 +361,17 @@ static int init_server(int* sock) {
 
 #endif //_WIN32
 
-int main (void) {
-	const char response_cors[] = "HTTP/1.1 204 No Content\r\n"
-	"Access-Control-Allow-Origin: *\r\n"
-	"Access-Control-Allow-Methods: POST, OPTIONS\r\n"
-	"Access-Control-Allow-Headers: content-type\r\n"
-	"Content-Length: 0\r\n"
-	"Connection: keep-alive\r\n"
-	"\r\n";
 
-	const char data_response[] = "HTTP/1.1 200 OK\r\n"
-	"Access-Control-Allow-Origin: *\r\n"
-	"Content-Length: 939\r\n"
-	"Connection: keep-alive\r\n"
-	"Content-Type: application/json\r\n"
-	"\r\n"
-	"["
-	"{\"city\":\"Perugia\",\"type\":1,\"temperature\":\"22°\",\"humidity\":\"69%\",\"pressure\":\"1015 hPa\",\"wind_velocity\":12,\"wind_direction\":\"120° SE\"},"
-    "{\"city\":\"Perugia\",\"type\":2,\"temperature\":\"-2°\",\"humidity\":\"80%\",\"pressure\":\"1420 hPa\",\"wind_velocity\":12,\"wind_direction\":\"110° E\"},"
-    "{\"city\":\"Perugia\",\"type\":3,\"temperature\":\"12°\",\"humidity\":\"40%\",\"pressure\":\"1069 hPa\",\"wind_velocity\":5,\"wind_direction\":\"130° NE\"},"
-    "{\"city\":\"Perugia\",\"type\":4,\"temperature\":\"5°\",\"humidity\":\"30%\",\"pressure\":\"1014 hPa\",\"wind_velocity\":120,\"wind_direction\":\"20° S\"},"
-    "{\"city\":\"Perugia\",\"type\":5,\"temperature\":\"22°\",\"humidity\":\"42%\",\"pressure\":\"1078 hPa\",\"wind_velocity\":12,\"wind_direction\":\"13° SW\"},"
-    "{\"city\":\"Perugia\",\"type\":1,\"temperature\":\"32°\",\"humidity\":\"69%\",\"pressure\":\"1015 hPa\",\"wind_velocity\":69,\"wind_direction\":\"12° NW\"},"
-    "{\"city\":\"Perugia\",\"type\":2,\"temperature\":\"-45°\",\"humidity\":\"80%\",\"pressure\":\"1420 hPa\",\"wind_velocity\":42,\"wind_direction\":\"71° W\"}"
-    "]";
+static const char response_cors[] = "HTTP/1.1 204 No Content\r\n"
+"Access-Control-Allow-Origin: *\r\n"
+"Access-Control-Allow-Methods: POST, OPTIONS\r\n"
+"Access-Control-Allow-Headers: content-type\r\n"
+"Content-Length: 0\r\n"
+"Connection: keep-alive\r\n"
+"\r\n";
+
+int main (void) {
+	srand(time(0));
 	
 	socket_zt sock = 0;
 	if (init_server(&sock) < 0) {
@@ -347,6 +415,7 @@ int main (void) {
 		}
 		
 		char* content = NULL;
+		int content_size = 0;
 
 		int request_type = 0;
 		unsigned long long int buf_cnt = 0;
@@ -354,7 +423,7 @@ int main (void) {
 			buf_cnt += ret;
 
 			int pos = 0;
-			if ((pos = str_tok(buffer, "\r\n\r\n")) >= 0) {
+			if ((pos = str_n_tok(buffer, "\r\n\r\n", buf_cnt)) >= 0) {
 				if ((request_type = check_request_type(buffer)) < 0) {
 					printf("Failed to check the request type.\n");
 					return 1;
@@ -367,20 +436,20 @@ int main (void) {
 					break;
 				}
 				
-				int content_len = 0;
-				if ((content_len = check_content_size(buffer)) < 0) {
+				content_size = 0;
+				if ((content_size = check_content_size(buffer, buf_cnt)) < 0) {
 					printf("\n\n-------\nBuffer(%llu): \"%s\"\n-------\n\n", buf_cnt, buffer);
 					free(buffer);
 					printf("An error occurred while checking the content length option field.\n");
 					return 1;
 				}
 				
-				if (content_len == 0) {
+				if (content_size == 0) {
 					free(buffer);
 					break;
 				}
 
-				content = (char*) calloc(content_len, sizeof(char));
+				content = (char*) calloc(content_size, sizeof(char));
 				if (content == NULL) {
 					free(buffer);
 					printf("An error occrured while allocating content buffer.\n");
@@ -391,7 +460,7 @@ int main (void) {
 				mem_cpy(content, buffer + pos + 4, already_received_content);
 				SAFE_FREE(buffer);
 				
-				int remaining_content = content_len - already_received_content;
+				int remaining_content = content_size - already_received_content;
 				if (remaining_content && (ret = recv(client_sock, content + already_received_content, remaining_content, 0)) != remaining_content) {
 					SAFE_FREE(content);
 					printf("ERROR: Expected %d bytes but received %d\n", remaining_content, ret);
@@ -418,16 +487,26 @@ int main (void) {
 		if (request_type == PREFLIGHT_CORS) {
 			int res_len = 0;
 			if ((res_len = send(client_sock, response_cors, sizeof(response_cors), 0)) != sizeof(response_cors)) {
-				if (res_len >= 0) printf("\nSent %d bytes out of %lu.\n", res_len, sizeof(response_cors));
+				if (res_len >= 0) printf("\nSent %d bytes out of %lu.\n", res_len, (unsigned long) sizeof(response_cors));
 				perror("Failed to send the response");
 				return 1;
 			}
+			
+			printf("Sent response back (%d)!\n", res_len);
+			
+			close_socket(client_sock);
+
+			printf("-------------------\n");
+			printf("Connection closed.\n");
+			printf("-------------------\n");
+			
 			continue;
 		}
 
 		char* city = NULL;
+		char* key = NULL;
 		unsigned char day = 0;
-		if (parse_json_request((const char*) content, &city, &day) < 0) {
+		if (parse_json_request((const char*) content, content_size, &city, &key, &day) < 0) {
 			printf("An error occurred while parsing the query.\n");
 			SAFE_FREE(content);
 			return 1;
@@ -435,14 +514,28 @@ int main (void) {
 		
 		SAFE_FREE(content);
 
-		printf("Queried, city: '%s', day: %u\n", city, day);
+		printf("Queried, city: '%s', day: %u with key: '%s'\n", city, day, key);
+		SAFE_FREE(key);
+		
+		char* response = NULL;
+		int response_size = generate_response(&response, city, day);
+	
+		SAFE_FREE(city);
+		
+		if (response_size < 0) {
+			printf("Failed to generate the response.\n");
+			return 1;
+		} 
 
 		int res_len = 0;
-		if ((res_len = send(client_sock, data_response, sizeof(data_response), 0)) != sizeof(data_response)) {
-			if (res_len >= 0) printf("\nSent %d bytes out of %lu.\n", res_len, sizeof(data_response));
+		if ((res_len = send(client_sock, response, response_size, 0)) != response_size) {
+			SAFE_FREE(response);
+			if (res_len >= 0) printf("\nSent %d bytes out of %d.\n", res_len, response_size);
 			perror("Failed to send the data_response");
 			return 1;
 		}
+		
+		SAFE_FREE(response);
 
 		printf("Sent response back (%d)!\n", res_len);
 
